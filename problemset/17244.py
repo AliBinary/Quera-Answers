4019d1acc8c2e2997b3d159fc5e2cad7b82fd290
AliBinary <AliGhanbariCs@gmail.com>
2023-09-10 23:34:18 +0330

    solve some easy questions

---
+++ problemset/17244.py
@@ -0,0 +1,12 @@
+'''
+    author:  AliTitan051
+    Email: AliGhanbariCs@gmail.com
+    GitHub: https://github.com/AliBinary
+    created: 10.09.2023 23:32:38
+'''
+
+k = int(input())
+ans = 0
+for i in range(0, k):
+    ans += i+1
+print(ans)
---
+++ problemset/3408.py
@@ -0,0 +1,11 @@
+'''
+    author:  AliTitan051
+    Email: AliGhanbariCs@gmail.com
+    GitHub: https://github.com/AliBinary
+    created: 10.09.2023 23:32:26
+'''
+
+input()
+lis = input().split()
+for i in range(len(lis)-1, -1, -1):
+    print(lis[i], end=' ')
