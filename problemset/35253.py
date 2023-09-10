507fd19102b05900e25674f4572d3ee03e0a8d16
AliBinary <AliGhanbariCs@gmail.com>
2023-09-10 23:42:43 +0330

    Solve one more question

---
+++ problemset/35253.py
@@ -0,0 +1,10 @@
+'''
+    author:  AliTitan051
+    Email: AliGhanbariCs@gmail.com
+    GitHub: https://github.com/AliBinary
+    created: 10.09.2023 23:41:44
+'''
+
+input()
+w = list(map(int, input().split()))
+print(w.index(max(w))+1)
