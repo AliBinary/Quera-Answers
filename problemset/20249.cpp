ce112505414f971697b15e000198b7007c50bc6c
AliBinary <AliGhanbariCs@gmail.com>
2024-02-27 00:22:14 +0330

    Solve a damn question

--- problemset/20249.cpp
+++ problemset/20249.cpp
@@ -2,34 +2,115 @@
     author:  Ali
     Email: AliGhanbariCs@gmail.com
     GitHub: https://github.com/AliBinary
-    created: 24.02.2024 21:22:12
+    created: 26.02.2024 10:42:44
 */
-
 #include <bits/stdc++.h>
 using namespace std;
 
+#define endl '\n'
+#define sep ' '
+typedef long long int ll;
+typedef long double ld;
+typedef string str;
+
+// pairs
+typedef pair<ld, ld> pdd;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef pair<pii, int> ppi;
+typedef pair<int, pii> pip;
+typedef pair<pii, pii> ppp;
+#define F first
+#define S second
+#define mp make_pair
+
+#define tcT template <class T
+tcT > using V = vector<T>;
+
+// vectors
+#define sz(x) int((x).size())
+#define bg(x) begin(x)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define sor(x) sort(all(x));
+#define sor_(x) sort(rall(x));
+#define rsz resize
+#define ins insert
+#define pb push_back
+#define eb emplace_back
+#define ft front()
+#define bk back()
+
+#define lb lower_bound
+#define ub upper_bound
+tcT > int lwb(V<T> &a, const T &b) { return int(lb(all(a), b) - bg(a)); }
+tcT > int upb(V<T> &a, const T &b) { return int(ub(all(a), b) - bg(a)); }
+
+// loops
+#define f(i, a, b) for (int i = (a); i < (b); ++i)
+#define f_(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define rep(n) f(_, 0, n)
+#define each(x, a) for (auto &x : a)
+
+// debug
+#define debug(x) cerr << #x << '=' << (x) << endl;
+#define debugp(x) cerr << #x << "= {" << (x.first) << ", " << (x.second) << "}" << endl;
+#define debug2(x, y) cerr << "{" << #x << ", " << #y << "} = {" << (x) << ", " << (y) << "}" << endl;
+#define debugv(v)             \
+    {                         \
+        cerr << #v << " : ";  \
+        for (auto x : v)      \
+            cerr << x << ' '; \
+        cerr << endl;         \
+    }
+#define wall cout << "----------------------------------------\n";
+#define kill(x) return cout << x << endl, 0
+
+// bitwise ops
+constexpr int pct(int x) { return __builtin_popcount(x); }               // # of bits set
+constexpr int bits(int x) { return x == 0 ? 0 : 31 - __builtin_clz(x); } // floor(log2(x))
+constexpr int p2(int x) { return 1 << x; }
+constexpr int msk2(int x) { return p2(x) - 1; }
+ll cdiv(ll a, ll b) { return a / b + ((a ^ b) > 0 && a % b); }                     // divide a by b rounded up
+ll fdiv(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); }                     // divide a by b rounded down
+tcT > bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }                // set a = min(a,b)
+tcT > bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }                // set a = max(a,b)
+tcT > void remDup(vector<T> &v) { sort(all(v)), v.erase(unique(all(v)), end(v)); } // sort and remove duplicates
+tcT > bool cmpFN(const T &a, const T &b) { return (a.second < b.second); }
+
+void solve();
 int main()
+{
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+void solve()
 {
     int n, k;
     cin >> n >> k;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++)
+
+    int sum = 0;
+    rep(n)
     {
-        cin >> a[i];
+        int x;
+        cin >> x;
+        sum += x;
     }
-    sort(a.begin(), a.end());
-    int ans = 0;
-    int x = 0, y = n - 1;
-    while (x < y)
-    {
-        if (a[x] + a[y] <= k)
-        {
-            ans++;
-            x += 1;
-            y -= 1;
-        }
-        else
-            y -= 1;
-    }
-    cout << ans;
+    int ans = ((n * k) - sum) / k;
+    cout << ans << endl;
 }
+
+/* stuff you should look for
+ * int overflow, array bounds
+ * special cases (n=1?)
+ * do smth instead of nothing and stay organized
+ * WRITE STUFF DOWN
+ * DON'T GET STUCK ON ONE APPROACH
+ */
